2678eefcd461f5f53e69bdb23f909d8ba1f1c61b
Matheus Menezes Sant'ana <matheusm.santana@outlook.com>
2020-03-03 11:36:26 -0300

    The TimeZone class was finalized

--- Project1 - Classes/bank_accounts.py
+++ Project1 - Classes/bank_accounts.py
@@ -1,6 +1,19 @@
-class TimeZone:
-    pass
-
+from .time_zone import TimeZone
 
 class Account:
-    pass
+
+    # time_zone_offset = timezone()
+    
+    def __init__(self, account_number, first_name, last_name):
+        self._account_number = account_number
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @property
+    def account_number(self):
+        return self._account_number
+
+    @account_number.setter
+    def account_number(self, value):
+        # if 
+        pass
--- Project1 - Classes/test_bank_accounts.py
+++ Project1 - Classes/test_bank_accounts.py
@@ -1,12 +1,13 @@
 from unittest import TestCase
 
-from bank_accounts import TimeZone, Account
+from .bank_accounts import Account
+from .time_zone import TimeZone
 
 
 class TestTimeZone(TestCase):
 
     def setUp(self) -> None:
-        self.t1 = TimeZone()
+        self.t1 = TimeZone(name='ABC', offset_hours=-2, offset_minutes=-7)
 
 
 class TestAccounts(TestCase):
